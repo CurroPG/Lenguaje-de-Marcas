d3a8f59cc509c5f78b25168ff3677266903d363a
Curro Portillo <211curroportillo@gmail.com>
2026-03-14 13:43:25 +0100

    cambios y creaciones

---
+++ JS/LMSGI_CurroPortilloGuerrero_UD04_Proyecto/js/javascript.js
@@ -0,0 +1,58 @@
+/**
+ * @file index.js
+ * @description Página principal de Studio Ghibli. Carga películas y personajes
+ *              desde la Ghibli API usando fetch y .then().
+ * @author TU_NOMBRE_AQUI
+ * @version 1.0
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // TODO: referencias a los botones y contenedores
+
+    // TODO: evento click para cargar películas
+
+    // TODO: evento click para cargar personajes
+
+
+    // ─────────────────────────────────────────────────────────
+
+    /**
+     * @function cargarPeliculas
+     * @description Hace una petición al endpoint /films de la Ghibli API.
+     * @returns {void}
+     */
+    function cargarPeliculas() {
+        // TODO
+    }
+
+    /**
+     * @function mostrarPeliculas
+     * @description Recibe el array de películas y las muestra en el contenedor.
+     * @param {Array} peliculas - Array de objetos película devueltos por la API
+     * @returns {void}
+     */
+    function mostrarPeliculas(peliculas) {
+        // TODO
+    }
+
+    /**
+     * @function cargarPersonajes
+     * @description Hace una petición al endpoint /people de la Ghibli API.
+     * @returns {void}
+     */
+    function cargarPersonajes() {
+        // TODO
+    }
+
+    /**
+     * @function mostrarPersonajes
+     * @description Recibe el array de personajes y los muestra en el contenedor.
+     * @param {Array} personajes - Array de objetos personaje devueltos por la API
+     * @returns {void}
+     */
+    function mostrarPersonajes(personajes) {
+        // TODO
+    }
+
+});
